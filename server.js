b16e9d3d2b53cee76700a9250ec700371bb6d1c3
John Tomaselli <jrt@gtz.com>
2013-07-01 15:26:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -64,30 +64,107 @@ app.server.listen(app.get('port'), function(){
 });
 
 
+var old_auth;
 
-console.log('==========================================');
-app.io.sockets.on('connection',socket);// function(socket));{JCFG});//,JCFG);
-//console.log('===============socket===========================',socket);
+old_auth = app.io.get('authorization');
 
+app.io.set("authorization", passportSocketIo.authorize({
+    passport: passport,
+    cookieParser: express.cookieParser, // problem
+    key:    'express.sid',
+    secret:  'mysecret',
+    store:   mySessionStore,
+    success: function(data, accept) {
+        return old_auth(data, accept);
+    }
+}));
+
+console.log('=below does not work if I use authorization=======');
+app.io.sockets.on('connection',socket);
+
+
+// old code
 // Socket.io Communication
-app.io.set('authorization', function (data, accept) {
-    if (!data.headers.cookie)
-        return accept('No cookie transmitted.  ', false);
-    data.cookie = require('cookie').parse(data.headers.cookie, SITE_SECRET);
-    data.sessionID = data.cookie['sid'].substring(2, 26);
-    console.log('data.cookie:', data.cookie);
-    console.log('data.sessionID: ', data.sessionID);
-    mySessionStore.get(data.sessionID, function (err, session) {
-        if (err) {
-            console.log('myStore.get return error: ', err);
-            accept(err.message, false)
-        } else if (!session) {
-            console.log('This Session not found. ');
-            return accept('Session not found.', false);
-        }
-        console.log('This session: ', session, data.sessionID);
-        data.session = session;
-        accept(null, true);
-    });
-});
+////app.io.configure(function (){
+//console.log('express.cookieParser ',express.cookieParser)
+//console.log('sessionConfig ',sessionConfig)
 
+//app.io.set( "authorization", passportSocketIo.authorize({
+//    passport: passport,
+//    cookieParser: express.cookieParser,
+//    key: config.session.key,
+//    secret: config.session.secret,
+//    store: config.session.store,
+//    success: (data, accept(old_auth, data, accept)
+//    }));
+//var old_auth;
+//
+//old_auth = app.io.get('authorization');
+//
+//app.io.set("authorization", passportSocketIo.authorize({
+//    passport: passport,
+//    cookieParser: express.cookieParser,
+//    key:    'express.sid',
+//    secret:  'mysecret',
+//    store:   mySessionStore,
+//    success: function(data, accept) {
+//        return old_auth(data, accept);
+//    }
+//}));
+//
+////    success: function(old_auth, accept){
+////    console.log("success socket.io auth");
+////    }
+//
+//
+////
+////    app.io.set("authorization", passportSocketIo.authorize({
+////        cookieParser:express.cookieParser,
+////        key:    'express.sid',
+////        secret: 'secret', //the session secret to parse the cookie
+////        store:   mySessionStore,     //the session store that express uses
+//////        fail: function(data, accept) {
+//////            // console.log("failed");
+//////            // console.log(data);// *optional* callbacks on success or fail
+//////            accept(null, false);             // second param takes boolean on whether or not to allow handshake
+//////        },
+////        success: function(data, accept) {
+//////            //  console.log("success socket.io auth");
+//////            //   console.log(data);
+////            accept(old_autg, true);
+////        }
+////    }));
+//////});
+//
+//
+//
+//console.log('4==========================================');
+//app.io.sockets.on('connection',socket);
+//
+//
+//
+//console.log('==========================================');
+//app.io.sockets.on('connection',socket);// function(socket));{JCFG});//,JCFG);
+////console.log('===============socket===========================',socket);
+//
+//// Socket.io Communication
+//app.io.set('authorization', function (data, accept) {
+//    if (!data.headers.cookie)
+//        return accept('No cookie transmitted.  ', false);
+//    data.cookie = require('cookie').parse(data.headers.cookie, SITE_SECRET);
+//    data.sessionID = data.cookie['sid'].substring(2, 26);
+//    console.log('data.cookie:', data.cookie);
+//    console.log('data.sessionID: ', data.sessionID);
+//    mySessionStore.get(data.sessionID, function (err, session) {
+//        if (err) {
+//            console.log('myStore.get return error: ', err);
+//            accept(err.message, false)
+//        } else if (!session) {
+//            console.log('This Session not found. ');
+//            return accept('Session not found.', false);
+//        }
+//        console.log('This session: ', session, data.sessionID);
+//        data.session = session;
+//        accept(null, true);
+//    });
+//});
